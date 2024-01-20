808aaab8522c26a9675c896bd6b5e83c72afdd82
Edward Doris <ed.doris@gmail.com>
2024-01-20 12:26:15 -0600

    set up logic for db

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 from config import Config
+from app.extensions import db
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     # Initialize Flask extensions here
+    db.init_app(app)
 
     # Register blueprints here
     from app.main import bp as main_bp
@@ -21,7 +23,6 @@ def create_app(config_class=Config):
     @app.route('/test/')
     def test_page():
         return '<h1>Testing the Flask Application Factory Pattern</h1>'
-
 
     return app
 
